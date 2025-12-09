ac5263b78127432b04affcf24c4776639dac5666
vedantparasharr <iemvedant@gmail.com>
2025-12-09 15:35:39 +0530

    Create a server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("I am working");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running at http://localhost:3000");
+});
